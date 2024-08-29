997c9f587b1677b39e36f35c567dd133b911b1a0
Hernando Orozco <hernandoorozco@outlook.es>
2024-08-29 11:46:21 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "primera tarea",
+    description: "Primera tarea",
+  },
+  {
+    id: 2,
+    title: "segunda tarea",
+    description: "segunda tarea",
+  },
+  {
+    id: 3,
+    title: "tercera tarea",
+    description: "tercera tarea",
+  },
+];
